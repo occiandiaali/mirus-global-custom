74ee6ad11c1c8242dc5d0faad3731eafd2ab454c
occiandiaali <ocean.diaali@gmail.com>
2020-07-25 18:04:13 +0100

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,18 +14,21 @@ class Directory extends React.Component {
           imageUrl:
             'https://cdn.pixabay.com/photo/2016/05/17/22/16/baby-1399332_960_720.jpg',
           id: 1,
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
           imageUrl:
             'https://cdn.pixabay.com/photo/2016/02/19/10/21/girl-1209193_960_720.jpg',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'shoes',
           imageUrl:
             'https://cdn.pixabay.com/photo/2016/11/19/11/33/footwear-1838767_960_720.jpg',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'shirts',
@@ -33,6 +36,7 @@ class Directory extends React.Component {
             'https://cdn.pixabay.com/photo/2016/11/22/19/08/blur-1850082_960_720.jpg',
           size: 'large',
           id: 4,
+          linkUrl: '',
         },
         {
           title: 'fragrances',
@@ -40,6 +44,7 @@ class Directory extends React.Component {
             'https://cdn.pixabay.com/photo/2019/04/06/19/20/oil-4108079_960_720.jpg',
           size: 'large',
           id: 5,
+          linkUrl: '',
         },
       ],
     };
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, size, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
