e030bf33359e4a226ee7552202fb5da747d127c1
occiandiaali <ocean.diaali@gmail.com>
2020-07-27 16:34:42 +0100

    add shop page, collection preview component, and collection item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className='collection-preview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {items // beware of perf bottleneck here, whenever component gets re-rendered
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
