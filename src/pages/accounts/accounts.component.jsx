929f4b8a7c73a2b3b80b675d4088adaf50062ec6
occiandiaali <ocean.diaali@gmail.com>
2020-07-28 12:51:58 +0100

    add custom button, form input, sign in and accounts components

---
+++ src/pages/accounts/accounts.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './accounts.styles.scss';
+
+const Accounts = () => {
+  return (
+    <div className='accounts'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default Accounts;
